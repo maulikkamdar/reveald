59f762cc2a24ce230f995f259380fd421c74a493
maulikkamdar <maulik@maulik-kamdar.com>
2014-03-16 18:02:32 +0000

    First Commit

---
+++ js/config.js
@@ -0,0 +1,35 @@
+
+var defaultID = ((readCookie('useModel')!= null && readCookie('useModel')!='') ? (readCookie('useModel')) : 11111);
+var defaultLoc = 'data/CancerChemoPrevention.owl';
+var uriStr = "http://srvgal78.deri.ie:8080/sparql"; /// put in your SPARQL Endpoint here
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
